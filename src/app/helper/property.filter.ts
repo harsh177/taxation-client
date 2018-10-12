cdbf6f804384958941ba37d45358caf48291b7b9
harsh177 <sharmaharsh177@gmail.com>
2018-10-12 17:57:13 +0530

    Harsh|filterChangesAndPattaChanges

--- src/app/helper/property.filter.ts
+++ src/app/helper/property.filter.ts
@@ -10,7 +10,7 @@ export class PropertyFilterPipe implements PipeTransform {
     if(!searchText) return items;
     searchText = searchText.toLowerCase();
     return items.filter( it => {
-        return it.propertyNumber.toLowerCase().includes(searchText) ||  it.subHolder.toLowerCase().includes(searchText) ||  it.area.toLowerCase().includes(searchText)  ||  it.city.toLowerCase().includes(searchText) ||  it.customUniqueId.toLowerCase().includes(searchText)   ||  it.samagraId.toLowerCase().includes(searchText);
+        return it.propertyNumber.toLowerCase().includes(searchText) ||  it.subHolder.toLowerCase().includes(searchText) ||  it.area.toLowerCase().includes(searchText)  ||  it.district.toLowerCase().includes(searchText) ||  it.customUniqueId.toLowerCase().includes(searchText)   ||  it.samagraId.toLowerCase().includes(searchText);
         });
    }
 }
